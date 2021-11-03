346e8f7999707ac02e4360cef531c742c9cbc42d
testasaad <asad2010222@gmail.com>
2021-11-03 21:41:46 +0400

    add job in views url

--- job/models.py
+++ job/models.py
@@ -14,9 +14,17 @@ class Job(models.Model): # table
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
-    #category = models.
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
     experience = models.IntegerField(default=1)
 
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+
+    def __str__(self):
+        return self.name
